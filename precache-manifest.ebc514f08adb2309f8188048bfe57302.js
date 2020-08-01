68ea2c37b225469734f9b83fcf102d5fc512848c
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-08-01 16:32:37 -0400

    Updates

---
+++ precache-manifest.ebc514f08adb2309f8188048bfe57302.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd963ee977e6f47c8fc188265cb21c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f6cb4a0aa39e01faa9c",
+    "url": "/static/css/2.c7661d7b.chunk.css"
+  },
+  {
+    "revision": "fa7740f8bdf169819649",
+    "url": "/static/css/main.b93ef79e.chunk.css"
+  },
+  {
+    "revision": "9f6cb4a0aa39e01faa9c",
+    "url": "/static/js/2.ae366e69.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ae366e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7740f8bdf169819649",
+    "url": "/static/js/main.50531df1.chunk.js"
+  },
+  {
+    "revision": "b1e42bd74b2d2274ea36",
+    "url": "/static/js/runtime-main.f7be1adb.js"
+  },
+  {
+    "revision": "6321d57907c28426346e4414051e62ad",
+    "url": "/static/media/Resume_Isaiah_Collazo.6321d579.pdf"
+  },
+  {
+    "revision": "47780f741fd327f65690205b0e8478a6",
+    "url": "/static/media/contact_background.47780f74.jpg"
+  },
+  {
+    "revision": "a99e47ea8727d827ec2ad2894c4dfa13",
+    "url": "/static/media/home_background.a99e47ea.jpg"
+  },
+  {
+    "revision": "b79d26663a978783e2364ae250eeb329",
+    "url": "/static/media/image_contact.b79d2666.JPG"
+  },
+  {
+    "revision": "6eca29eac2b4f83c0ca6c4c138f779cd",
+    "url": "/static/media/image_me.6eca29ea.jpg"
+  },
+  {
+    "revision": "8ca3e748e67a7a190d1dc7c9ac39bfac",
+    "url": "/static/media/logo.8ca3e748.png"
+  }
+]);
